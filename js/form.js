5d43a5a2a794283a695b2c180e5f0a3ce1342a92
JessBob <54863103+Jessbob@users.noreply.github.com>
2020-02-07 15:29:57 -0600

    added aws lambda serverless backend for contact form

---
+++ js/form.js
@@ -0,0 +1,31 @@
+(() => {
+  const form = document.querySelector("form");
+  const formResponse = document.querySelector("js-form-response");
+
+  form.onsubmit = e => {
+    e.preventDefault();
+
+    const data = {};
+    const formElements = Array.from(form);
+    formElements.map(input => (data[input.name] = input.value));
+
+    console.log(JSON.stringify(data));
+
+    var xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action, true);
+    xhr.setRequestHeader("Accept", "application/json; charset=utf-8");
+    xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+
+    xhr.send(JSON.stringify(data));
+
+    xhr.onloadend = response => {
+      if (response.target.status === 200) {
+        form.reset();
+        formResponse.innerHTML = "Thanks for the message!";
+      } else {
+        formResponse.innerHTML = "Something went wrong";
+        console.error(JSON.parse(response.target.response).message);
+      }
+    };
+  };
+})();
